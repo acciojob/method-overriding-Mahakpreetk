ac9cb2fc8d860b8abd14f7084a7b72e900404d4d
Mahakpreet Kaur <kaurmahakpreet0520@gmail.com>
2023-03-20 21:33:33 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]){
+        B b=new B();
+        String a=b.meth();
+        System.out.println(a);
+        String c=b.meth();
+        System.out.println(c);
+    }
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+     @Override
+        public String meth(){
+         return "Invoking method from class A";
+     }
+    }
 }
